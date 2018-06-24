6504d07c02cfd8adf0d597ed559f3382f907d256
kozyszoo <k.yamaoka0919@gmail.com>
2018-06-24 13:34:26 +0900

    3-1. First steo of Developing by Scaffolding

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180624031516) do
+ActiveRecord::Schema.define(version: 20180624042948) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
